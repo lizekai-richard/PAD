ea241dd5ba371da0d092cab5bc6debcf3f95dfa6
lizekai-richard <lzknus@gmail.com>
2024-03-13 15:31:13 +0800

    update

--- scripts/buffer.sh
+++ scripts/buffer.sh
@@ -6,14 +6,14 @@ nohup python buffer_DQ.py \
 --train_epochs=100 \
 --num_experts=100 \
 --zca \
---buffer_path="/home/kwang/big_space/results/lzk/buffer_storage/avg_loss_sorted/" \
+--buffer_path="../buffer_storage/convnet_loss_sorted/" \
 --data_path="../dataset/" \
 --bins_path="../bins/" \
---sort_method="CIFAR10_AvgModels" \
+--sort_method="CIFAR10_ConvNet" \
 --rho_max=0.01 \
 --rho_min=0.01 \
 --alpha=0.3 \
 --lr_teacher=0.01 \
 --mom=0. \
 --batch_train=256 \
-> ../logs/train_teacher_trajectories_cl_v3_avg_models_based.log 2>&1 &
+> ../logs/train_teacher_trajectories_cl_v2_convnet_loss_based.log 2>&1 &
