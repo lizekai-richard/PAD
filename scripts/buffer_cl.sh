d9b64f60da78bb5f726678827cd5c59a703bd7d4
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2024-04-29 16:58:38 +0800

    Update buffer_cl.sh

--- scripts/buffer_cl.sh
+++ scripts/buffer_cl.sh
@@ -3,11 +3,11 @@ nohup python buffer_CL.py \
 --dataset=CIFAR10 \
 --model=ConvNet \
 --train_epochs=100 \
---num_experts=50 \
+--num_experts=100 \
 --zca \
---buffer_path="/home/wangkai/big_space/lzk/buffer_storage/cl_75_30/" \
+--buffer_path="/home/wangkai/big_space/lzk/buffer_storage/cl_grand_75_40_01/" \
 --data_path="../dataset/" \
---sort_method="CIFAR10_Uncertainty" \
+--sort_method="CIFAR10_GraNd" \
 --rho_max=0.01 \
 --rho_min=0.01 \
 --alpha=0.3 \
@@ -15,10 +15,6 @@ nohup python buffer_CL.py \
 --mom=0. \
 --batch_train=256 \
 --init_ratio=0.75 \
---end_epoch=30 \
---rm_easy='False' \
---add_hard='True' \
---rm_start=50 \
---rate=0.01 \
---max_ratio=0.2 \
-> ../logs/train_teacher_trajectories_cifar10_cl_75_30.log 2>&1 &
+--add_end_epoch=40 \
+--rm_easy_ratio=0.1 \
+> ../logs/train_teacher_trajectories_cifar10_cl_grand_75_40_01.log 2>&1 &
